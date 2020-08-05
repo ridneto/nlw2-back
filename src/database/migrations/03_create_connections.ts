9e6c9742458bc444480e29dc04977ffdb4be01a7
ridneto <ridineu@hotmail.com>
2020-08-05 00:05:05 -0300

    backend nlw2

---
+++ src/database/migrations/03_create_connections.ts
@@ -0,0 +1,24 @@
+import Knex, { SchemaBuilder } from 'knex';
+
+export async function up(knex: Knex): Promise<SchemaBuilder> {
+    return knex.schema.createTable('connections', table => {
+        table.increments('id').primary();
+
+        table
+            .integer('user_id')
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+
+        table
+            .timestamp('created_at')
+            .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+            .notNullable();
+    });
+}
+
+export async function down(knex: Knex): Promise<SchemaBuilder> {
+    return knex.schema.dropTable('connections');
+}
