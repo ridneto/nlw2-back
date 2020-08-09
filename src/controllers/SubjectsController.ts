570f64eea02f1366a85e422182d1d60bb8248b98
ridneto <ridineu@hotmail.com>
2020-08-09 01:11:55 -0300

    Alterado estrutura rota assuntos

--- src/controllers/SubjectsController.ts
+++ src/controllers/SubjectsController.ts
@@ -3,7 +3,12 @@ import { Request, Response } from 'express';
 export default class SubjectsController {
     async index(request: Request, response: Response): Promise<Response> {
         return response.json({
-            subjects: ['Matemática', 'Inglês', 'Português', 'Geografia'],
+            subjects: [
+                { ds_subject: 'Matemática', id_subject: 'math' },
+                { ds_subject: 'Inglês', id_subject: 'ingles' },
+                { ds_subject: 'Português', id_subject: 'port' },
+                { ds_subject: 'Geografia', id_subject: 'geo' },
+            ],
         });
     }
 }
